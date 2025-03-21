8cc26cee7dbf2c3249931c9d94e8a4aef8acdf1f
Victor Andrade <victor64gaming@gmail.com>
2025-03-21 17:02:34 -0300

    Added models

---
+++ app/Models/Lesson.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lessons';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'room_id',
+        'timeslot_id',
+        'teacher_id',
+        'student_group_id',
+        'subject_id'
+    ];
+}
---
+++ app/Models/Timeslot.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Timeslot extends Model
+{
+    use HasFactory;
+
+    protected $table = 'timeslots';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'day_of_week',
+        'start_time',
+        'end_time',
+    ];
+}
