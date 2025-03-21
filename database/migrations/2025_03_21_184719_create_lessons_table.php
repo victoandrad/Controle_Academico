a1626fc1d0838ca744315104b5876b8a72ce34f9
Victor Andrade <victor64gaming@gmail.com>
2025-03-21 15:49:52 -0300

    Added migrations

---
+++ database/migrations/2025_03_21_184719_create_lessons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedInteger('room_id');
+            $table->unsignedInteger('timeslot_id');
+            $table->unsignedInteger('teacher_id');
+            $table->unsignedInteger('student_group_id');
+            $table->unsignedInteger('subject_id');
+
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->foreign('timeslot_id')->references('id')->on('timeslots');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->foreign('student_group_id')->references('id')->on('student_groups');
+            $table->foreign('subject_id')->references('id')->on('subjects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
