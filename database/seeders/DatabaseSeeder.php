5215871b0c4ab1e419a3ff766aa44f6b52db2429
Victor Andrade <victor64gaming@gmail.com>
2025-03-21 17:44:12 -0300

    Student seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 //            UserSeeder::class,
 //            TeacherSeeder::class,
 //            RoomSeeder::class,
+//            StudentSeeder::class,
         ]);
     }
 }
