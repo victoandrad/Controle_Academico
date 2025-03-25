f74c076edef98af14f5ceb3618ee40eaaa5c2789
Victor Andrade <victor64gaming@gmail.com>
2025-03-24 22:00:40 -0300

    Lesson seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             TaskSeeder::class,
             TeacherSeeder::class,
             TimeslotSeeder::class,
+            LessonSeeder::class,
         ]);
     }
 }
