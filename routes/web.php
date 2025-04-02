a24aebbe58e601a97fa4819eef1f6f3cd42da64f
Victor Andrade <victor64gaming@gmail.com>
2025-04-02 20:08:17 -0300

    Modified routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\CurriculumUnitController;
+use App\Http\Controllers\FrequencyController;
+use App\Http\Controllers\LessonController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentGroupController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\TimeslotController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('curriculumUnits', CurriculumUnitController::class);
+Route::resource('frequencies', FrequencyController::class);
+Route::resource('lessons', LessonController::class);
+Route::resource('rooms', RoomController::class);
+Route::resource('students', StudentController::class);
+Route::resource('studentGroups', StudentGroupController::class);
+Route::resource('tasks', TaskController::class);
+Route::resource('teachers', TeacherController::class);
+Route::resource('timeslots', TimeslotController::class);
+Route::resource('users', UserController::class);
