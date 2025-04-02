bf4d54ce46106f5442ef53be5fc32ea1b76e5f61
Victor Andrade <victor64gaming@gmail.com>
2025-04-02 20:07:42 -0300

    Added Student controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Student::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([]);
+        $data = Student::query()->create($validated);
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $data = Student::query()->findOrFail($id);
+            return response()->json($data);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([]);
+        try {
+            $data = Student::query()->findOrFail($id)->update($validated);
+            return response()->json($data);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            Student::query()->findOrFail($id)->delete();
+            return response()->noContent();
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+}
