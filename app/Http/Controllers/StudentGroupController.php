f3b7373d0c219ef09d361e2663362021dc09c8fe
Victor Andrade <victor64gaming@gmail.com>
2025-04-02 20:07:49 -0300

    Added StudentGroup controller

---
+++ app/Http/Controllers/StudentGroupController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentGroup;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class StudentGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = StudentGroup::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255|unique:student_groups,name',
+            'size' => 'required|integer|min:1',
+        ]);
+        $data = StudentGroup::query()->create($validated);
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $data = StudentGroup::query()->findOrFail($id);
+            return response()->json($data);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255|unique:student_groups,name',
+            'size' => 'required|integer|min:1',
+        ]);
+        try {
+            $data = StudentGroup::query()->findOrFail($id)->update($validated);
+            return response()->json($data);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            StudentGroup::query()->findOrFail($id)->delete();
+            return response()->noContent();
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+}
