6a79fd351977445e8d04f831da4c94624b1ed59b
Victor Andrade <victor64gaming@gmail.com>
2025-04-02 20:08:05 -0300

    Added Timeslot controller

---
+++ app/Http/Controllers/TimeslotController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Timeslot;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class TimeslotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Timeslot::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'day_of_week' => 'required|string|max:20',
+            'start_time' => 'required|date_format:H:i',
+            'end_time' => 'required|date_format:H:i|after:start_time',
+        ]);
+        $data = Timeslot::query()->create($validated);
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $data = Timeslot::query()->findOrFail($id);
+            return response()->json($data);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'day_of_week' => 'required|string|max:20',
+            'start_time' => 'required|date_format:H:i',
+            'end_time' => 'required|date_format:H:i|after:start_time',
+        ]);
+        try {
+            $data = Timeslot::query()->findOrFail($id)->update($validated);
+            return response()->json($data);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        try {
+            Timeslot::query()->findOrFail($id)->delete();
+            return response()->noContent();
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => 'Resource not found'
+            ], 404);
+        }
+    }
+}
